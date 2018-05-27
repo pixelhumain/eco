45415e8512c3bbfd13b344137e07438bd373f38a
Oceatoon <oceatoon@gmail.com>
2018-05-27 09:41:23 +0400

    custom small changes

--- views/custom/leport.php
+++ views/custom/leport.php
@@ -1,5 +1,18 @@
-<div class="col-xs-12" style="background-color: : white">
+<div class="col-xs-12" style="background-color: white">
 	
   <h1 class="homestead text-dark">Le Port : Emploi, Formation, Stage</h1>
+  <div class="col-xs-10">
   <img class="img-responsive" src='<?php echo $this->module->assetsUrl; ?>/images/custom/leport/banner.png'> 
+  </div>
+  <div class="col-xs-2" >
+  	<img class="img-responsive" src='<?php echo $this->module->assetsUrl; ?>/images/custom/leport/LOGO.jpg'> 
+  	<h2>Un ville Dynamique</h2>
+  	<p style="overflow-y: hidden;height: 200px;">
+  	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
 </div>
